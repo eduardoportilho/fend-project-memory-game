536b221f5fd2c91cbaff35d37099c6c2eb3e5e64
Eduardo Pilla Portilho <eduardo@portilho.com>
2018-06-30 17:51:36 +0200

    Basic game play

--- js/app.js
+++ js/app.js
@@ -1,8 +1,69 @@
-/*
- * Create a list that holds all of your cards
- */
+const deck = document.querySelector('.deck');
+const cardsSymbols = [
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-bomb',
+    'fa-leaf',
+    'fa-bicycle',
+]
+let blockCardFlip = false;
 
+function generateCardHtml(symbol) {
+    return `<li class="card" data-card="${symbol}"><i class="fa ${symbol}"></i></li>`
+}
 
+function resetCards() {
+    const cards = cardsSymbols.concat(cardsSymbols);
+    const shuffled = shuffle(cards);
+    const deckHtml = shuffled.map(generateCardHtml).join('');
+    deck.innerHTML = deckHtml;
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('click', onCardClick);
+    });
+}
+
+function onCardClick({ target: card }) {
+    if (card.classList.contains('open') || 
+        card.classList.contains('match') || 
+        blockCardFlip) {
+        return;
+    }
+    card.classList.add('open', 'show');
+    checkForMatches();
+}
+
+function checkForMatches() {
+    const openCards = document.querySelectorAll('.card.open');
+    if (openCards.length < 2) {
+        return;
+    }
+    const [card1, card2] = openCards;
+    if (card1.dataset.card === card2.dataset.card) {
+        markMatchedCards([card1, card2]);
+    } else {
+        blockCardFlip = true;
+        setTimeout(() => {
+            closeCards(openCards)
+            blockCardFlip = false;
+        }, 500);
+    }
+}
+
+function markMatchedCards(cards) {
+    closeCards(cards);
+    cards.forEach(card => {
+        card.classList.add('match');
+    });
+}
+
+function closeCards(cards) {
+    cards.forEach(card => {
+        card.classList.remove('open', 'show');
+    });
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -25,6 +86,12 @@ function shuffle(array) {
     return array;
 }
 
+function init() {
+    document.querySelector('.restart').addEventListener('click', resetCards);
+    resetCards();
+}
+
+init();
 
 /*
  * set up the event listener for a card. If a card is clicked:
